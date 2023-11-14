1a845128eb4dace970f90b3a235b9773d4ae3fee
Raz-kun <147388441+Raz-kun@users.noreply.github.com>
2023-11-15 01:15:24 +0800

    Grad Fortification Intergration

---
+++ Missionframework/modules/grad-fortifications/FortifyList.hpp
@@ -0,0 +1,70 @@
+//Fortification Items
+
+//Unit							||Classname									||Amount				
+//-------------------------------------------------------------------------------------------------------------------
+//Sandbags	
+[this,						"Land_BagFence_Long_F"						,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_HBarrier_5_F"							,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_HBarrierWall6_F"						,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_BagFence_Round_F"						,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_BagFence_01_round_green_F"			,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_BagFence_01_long_green_F"				,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_SandbagBarricade_01_half_F"			,5] call grad_fortifications_fnc_addFort;
+[this,						"wx_sandbag02"								,5] call grad_fortifications_fnc_addFort;
+[this,						"wx_sandbag03_mid"							,5] call grad_fortifications_fnc_addFort;
+[this,						"LAND_sb_revetment"							,5] call grad_fortifications_fnc_addFort;
+[this,						"LAND_Revetment_5"							,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_fortified_nest_small_ep1"				,5] call grad_fortifications_fnc_addFort;
+[this,						"LAND_mortarpit_sb"							,5] call grad_fortifications_fnc_addFort;
+[this,						"uns_sbagwall_u"							,5] call grad_fortifications_fnc_addFort;
+
+//Lights
+[this,						"Land_PortableLight_double_F"				,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_PortableLight_single_F"				,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_LampHalogen_F"						,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_LampShabby_F"							,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_TentLamp_01_suspended_F"				,5] call grad_fortifications_fnc_addFort;
+[this,						"PortableHelipadLight_01_blue_F"			,5] call grad_fortifications_fnc_addFort;
+[this,						"PortableHelipadLight_01_green_F"			,5] call grad_fortifications_fnc_addFort;
+[this,						"PortableHelipadLight_01_red_F"				,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_Campfire_burning"						,5] call grad_fortifications_fnc_addFort;
+
+//Buildings
+[this,						"LAND_uns_westbunker2"						,1] call grad_fortifications_fnc_addFort;
+[this,						"LAND_uns_westbunker1"						,1] call grad_fortifications_fnc_addFort;
+[this,						"Land_Wood_Tower"							,1] call grad_fortifications_fnc_addFort;
+[this,						"Razorwire"									,1] call grad_fortifications_fnc_addFort;
+[this,						"LAND_tower_1"								,1] call grad_fortifications_fnc_addFort;
+[this,						"LAND_uns_tentbarracks"						,1] call grad_fortifications_fnc_addFort;
+[this,						"LAND_UNS_OM"								,1] call grad_fortifications_fnc_addFort;
+[this,						"US_Tent_MASH"								,1] call grad_fortifications_fnc_addFort;
+[this,						"Land_camonet_east_ep1"						,1] call grad_fortifications_fnc_addFort;
+[this,						"Land_camonet_east_var1_ep1"				,1] call grad_fortifications_fnc_addFort;
+[this,						"Land_guardShed"							,1] call grad_fortifications_fnc_addFort;
+[this,						"Land_fort_rampart"							,1] call grad_fortifications_fnc_addFort;
+[this,						"LAND_P_wire1"								,1] call grad_fortifications_fnc_addFort;
+[this,						"LAND_P_wire1a"								,1] call grad_fortifications_fnc_addFort;
+[this,						"Land_Razorwire_F"							,1] call grad_fortifications_fnc_addFort;
+[this,						""											,1] call grad_fortifications_fnc_addFort;
+[this,						""											,1] call grad_fortifications_fnc_addFort;
+[this,						""											,1] call grad_fortifications_fnc_addFort;
+[this,						""											,1] call grad_fortifications_fnc_addFort;
+
+[this,						"TargetP_Inf4_Acc2_F"						,5] call grad_fortifications_fnc_addFort;
+[this,						"TargetP_Inf7_Acc2_F"						,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_Target_Pistol_01_F"					,5] call grad_fortifications_fnc_addFort;
+[this,						"Land_Target_Line_PaperTargets_01_F"		,5] call grad_fortifications_fnc_addFort;
+//[this,						""											,1] call grad_fortifications_fnc_addFort;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Missionframework/modules/grad-fortifications/cfgGradFortifications.hpp
@@ -0,0 +1,44 @@
+    buildTimeFactor = 0.5;
+	packUpTimeFactor = 0.25;
+	demolishTimeFactor = 0.25;
+    canDemolishDefault = 1;
+    playerInventorySize = 150;
+    vehicleInventorySizeFactor = 2;
+	fortificationOwnerType = "SIDE";
+	
+
+    class Fortifications {
+		class Land_TentLamp_01_suspended_F{
+			canFloat = 1;
+			canCollide = 1;
+		};
+	};	
+/*		
+        class Land_BagFence_Long_F {
+            boundingBoxSize = 0.8;
+            canFloat = 1;
+            canCollide = 1;
+            size = 3;
+            demolitionTime = 5;
+        };
+
+        class Land_HBarrier_5_F {
+            boundingBoxSize = 0.8;
+            surfaceNormal = 0;
+			canCollide = 1;
+        };
+		
+		
+    
+
+	//For Vehicle inventory
+    class Vehicles {
+        class B_Truck_01_box_F {
+            isStorage = 1;
+            vehicleInventorySize = 30;
+        };
+	//Add cargo containers (slingload container), Transport Truck, Chinook,	
+	
+    };
+	
+*/
